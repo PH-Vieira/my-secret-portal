75d04fbb7913d7a6dbddeb4e00e70503b6523444
PH-Vieira <vieira.pedroh2001@gmail.com>
2026-01-05 14:13:09 -0300

    allow all hosts

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   preview: {
-    allowedHosts: ['www.monumental.center', 'monumental.center']
+    // allowedHosts: ['www.monumental.center', 'monumental.center']
+    allowedHosts: "all"
   }
 }));
